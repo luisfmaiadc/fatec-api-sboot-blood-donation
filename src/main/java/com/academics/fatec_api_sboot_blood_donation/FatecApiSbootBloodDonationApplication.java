df64f21bd01a834cced4066593ac5aea47be3abd
luisfmaiadc <lf.mcosta23@gmail.com>
2024-11-21 23:37:44 -0300

    Inicializando repositório.

---
+++ src/main/java/com/academics/fatec_api_sboot_blood_donation/FatecApiSbootBloodDonationApplication.java
@@ -0,0 +1,13 @@
+package com.academics.fatec_api_sboot_blood_donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecApiSbootBloodDonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatecApiSbootBloodDonationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academics/fatec_api_sboot_blood_donation/FatecApiSbootBloodDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academics.fatec_api_sboot_blood_donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatecApiSbootBloodDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
